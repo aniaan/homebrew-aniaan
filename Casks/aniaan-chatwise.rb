3e5f8f6712c090ec3bd9f0b7a7fbc3bad97ce907
aniaan <hi@aniaan.dev>
2025-01-24 14:04:10 +0800

    update

--- Casks/aniaan-chatwise.rb
+++ Casks/aniaan-chatwise.rb
@@ -2,7 +2,8 @@ cask "aniaan-chatwise" do
   arch arm: "aarch64", intel: "x64"
 
   version "0.6.15"
-  sha256 :no_check
+  sha256 arm:   "c8b93e14fc73892004ea1454ba2c5e836aec4bd12fea3058e9c7345e516b379d",
+          intel: "da8c628c5ddb6e22b21f340828641bd4bc8c01a119ed289723eab44d614cbc6c"
 
   url "https://github.com/egoist/chatwise-releases/releases/download/v#{version}/ChatWise_#{version}_#{arch}.dmg"
 
