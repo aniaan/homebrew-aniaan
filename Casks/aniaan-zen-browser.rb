3782d74f3b715b5c70e091152f46c28b487eec28
aniaan <hi@aniaan.dev>
2025-01-31 21:41:21 +0800

    zen-browser

---
+++ Casks/aniaan-zen-browser.rb
@@ -0,0 +1,25 @@
+cask "aniaan-zen-browser" do
+  version "1.7.4b"
+  sha256 "4ac808cfdc82f97936b4deca8e61330fb08a2121f5c654e788ccf0d0b828af07"
+
+  url "https://github.com/zen-browser/desktop/releases/download/#{version}/zen.macos-universal.dmg",
+      verified: "github.com/zen-browser/desktop/"
+  name "aniaan-zen-browser"
+  desc "aniaan-zen-browser"
+  homepage "https://zen-browser.app/"
+
+  auto_updates true
+  depends_on macos: ">= :ventura"
+
+
+  app "Zen Browser.app"
+
+  zap trash: [
+        "~/Library/Application Support/zen",
+        "~/Library/Caches/Mozilla/updates/Applications/Zen Browser",
+        "~/Library/Caches/zen",
+        "~/Library/Preferences/org.mozilla.com.zen.browser.plist",
+        "~/Library/Saved Application State/org.mozilla.com.zen.browser.savedState",
+      ],
+      rmdir: "~/Library/Caches/Mozilla"
+end
