d809e562aa8a512e638b5defb69b98af3740beb5
aniaan <hi@aniaan.dev>
2025-02-05 14:37:15 +0800

    update

--- Casks/aniaan-obsidian.rb
+++ Casks/aniaan-obsidian.rb
@@ -2,7 +2,7 @@ cask "aniaan-obsidian" do
   version "1.8.4"
   sha256 "920d201f82d6efcb8a5319ef135086f01d41bc9cf23bcbe53fab5a2ef986c3fb"
 
-  url "https://github.com/obsidianmd/obsidian-releases/releases/download/v#{version}/Obsidian-#{version}.dmg"
+  url "https://github.com/obsidianmd/obsidian-releases/releases/download/v#{version}/Obsidian-#{version}.dmg",
       verified: "github.com/obsidianmd/"
   name "aniaan-obsidian"
   desc "aniaan-obisidian"
