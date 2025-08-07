8eb814f1b94d58c0f25e52225c7b962f16b896ba
aniaan <hi@aniaan.dev>
2025-08-07 16:40:32 +0800

    1password

---
+++ Casks/aniaan-1password.rb
@@ -0,0 +1,45 @@
+cask "aniaan-1password" do
+
+  version "latest"
+  sha256  "386cb24c010b4c668448e785e2bf04e8525c26ec2a4639765470a061a35458f3"
+
+  url "https://downloads.1password.com/mac/1Password.zip"
+  name "aniaan-1password"
+  desc "aniaan-1password"
+  homepage "https://1password.com/"
+
+
+  depends_on macos: ">= :monterey"
+
+  app "1Password.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/2BUA8C4S2C.com.1password*",
+    "~/Library/Application Scripts/2BUA8C4S2C.com.agilebits",
+    "~/Library/Application Scripts/com.1password.1password-launcher",
+    "~/Library/Application Scripts/com.1password.browser-support",
+    "~/Library/Application Support/1Password",
+    "~/Library/Application Support/Arc/User Data/NativeMessagingHosts/com.1password.1password.json",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.1password.1password.sfl*",
+    "~/Library/Application Support/CrashReporter/1Password*",
+    "~/Library/Application Support/Google/Chrome Beta/NativeMessagingHosts/com.1password.1password.json",
+    "~/Library/Application Support/Google/Chrome Canary/NativeMessagingHosts/com.1password.1password.json",
+    "~/Library/Application Support/Google/Chrome Dev/NativeMessagingHosts/com.1password.1password.json",
+    "~/Library/Application Support/Google/Chrome/NativeMessagingHosts/com.1password.1password.json",
+    "~/Library/Application Support/Microsoft Edge Beta/NativeMessagingHosts/com.1password.1password.json",
+    "~/Library/Application Support/Microsoft Edge Canary/NativeMessagingHosts/com.1password.1password.json",
+    "~/Library/Application Support/Microsoft Edge Dev/NativeMessagingHosts/com.1password.1password.json",
+    "~/Library/Application Support/Microsoft Edge/NativeMessagingHosts/com.1password.1password.json",
+    "~/Library/Application Support/Mozilla/NativeMessagingHosts/com.1password.1password.json",
+    "~/Library/Application Support/Vivaldi/NativeMessagingHosts/com.1password.1password.json",
+    "~/Library/Containers/2BUA8C4S2C.com.1password.browser-helper",
+    "~/Library/Containers/com.1password.1password*",
+    "~/Library/Containers/com.1password.browser-support",
+    "~/Library/Group Containers/2BUA8C4S2C.com.1password",
+    "~/Library/Group Containers/2BUA8C4S2C.com.agilebits",
+    "~/Library/Logs/1Password",
+    "~/Library/Preferences/com.1password.1password.plist",
+    "~/Library/Preferences/group.com.1password.plist",
+    "~/Library/Saved Application State/com.1password.1password.savedState",
+  ]
+end
