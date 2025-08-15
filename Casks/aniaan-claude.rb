869886bff257f6048e568fcf75a832ccdacc03fc
aniaan <hi@aniaan.dev>
2025-08-15 16:35:49 +0800

    f

--- Casks/aniaan-claude.rb
+++ Casks/aniaan-claude.rb
@@ -2,7 +2,7 @@ cask "aniaan-claude" do
   version "0.12.55,d55c63cae429e48c72cd9f306dbf2880a694e8fe"
   sha256 "d2c63df03ecd106ed773be18c63ba12b624e1bb6db019017e8f5caf928bbca10"
 
-  url "https://storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest/Claude.dmg"
+  url "https://storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest/Claude.dmg",
       verified: "storage.googleapis.com/osprey-downloads-c02f6a0d-347c-492b-a752-3e0651722e97/nest/"
   name "aniaan-claude"
   desc "aniaan-claude"
